a3302a7d946d36db0654a1f545ff6b83f0c92c63
dodonut <viniciusvims@gmail.com>
2018-06-13 22:42:31 -0300

    Finished

--- 6.00/ps1/ps1c.py
+++ 6.00/ps1/ps1c.py
@@ -14,21 +14,28 @@ n_steps = 0
 able_to_pay = True
 low = 0
 high = 10000
-m = high
+epsilon = 100
 
-
-while able_to_pay:
-    portion_saved = round(m/(annual_salary/12.0), 2)
-    if portion_saved > 1 : portion_saved = 1
+while abs(down_payment - current_savings) > epsilon:
+    current_savings = 0
+    n_months = 0
+    annual_salary = starter_salary
+    m = (low + high) / 2.0
+    portion_saved = round(m/(annual_salary/12.0), 4)
     while current_savings < down_payment and n_months < three_years:
         current_savings += (current_savings * r + portion_saved * annual_salary) / 12.0
         n_months += 1
         if n_months % 6 == 0:
             annual_salary += annual_salary * semi_annual_raise
 
-    m = (low + high) / 2.0
     if current_savings < down_payment:
-        able_to_pay = False
-    elif n_months <
+        low = m
+    else:
+        high = m
+    n_steps += 1
 
-print("Number of months: ", n_months)
+if portion_saved > 1:
+    print("It is not possible to pay the down payment in three years.")
+else:
+    print("Best savings rate: " , portion_saved)
+    print("Steps in bisection search: ", n_steps)
